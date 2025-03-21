bffb72321a1dec3a4343eac3469e74179a1151f2
iCastro117 <estelaramos391@gmail.com>
2025-03-20 21:19:44 -0500

    Primer commit en TAREAS-HTML-CSS

---
+++ assets/js/main.js
@@ -0,0 +1,44 @@
+(function() {
+    "use strict";
+
+    /**
+     * Easy selector helper function
+     */
+    const select = (el, all = false) => {
+        el = el.trim()
+        if (all) {
+            return [...document.querySelectorAll(el)]
+        } else {
+            return document.querySelector(el)
+        }
+    }
+
+
+
+
+    /**
+     * Initiate Datatables
+     */
+    const datatables = select('.datatable', true)
+    datatables.forEach(datatable => {
+        new simpleDatatables.DataTable(datatable, {
+            perPageSelect: [5, 10, 15, ["All", -1]],
+            columns: [{
+                    select: 2,
+                    sortSequence: ["desc", "asc"]
+                },
+                {
+                    select: 3,
+                    sortSequence: ["desc"]
+                },
+                {
+                    select: 4,
+                    cellClass: "green",
+                    headerClass: "red"
+                }
+            ]
+        });
+    })
+
+
+})();
